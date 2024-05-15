499d1960beb59909a1cf0351ea09632bf0c021b7
MattyMyers22 <mmyers2206@gmail.com>
2024-05-15 16:43:15 -0700

    Set print to values

--- scripts/extract.py
+++ scripts/extract.py
@@ -55,10 +55,7 @@ def main():
       print("No data found.")
       return
 
-    print("Name, Major:")
-    for row in values:
-      # Print columns A and E, which correspond to indices 0 and 4.
-      print(f"{row[0]}, {row[4]}")
+    print(values)
   except HttpError as err:
     print(err)
 
