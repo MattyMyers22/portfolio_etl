656bd477b9996f859da93bd32679f6b792401801
MattyMyers22 <mmyers2206@gmail.com>
2024-05-27 16:12:37 -0700

    Initial transform script with pseudo-code

---
+++ scripts/transform.py
@@ -0,0 +1,20 @@
+# Script to perform data cleaning and transformation of stock portfolio data
+# Saves the clean version of Excels as new copies
+
+# Author: Matthew Myers
+
+# Import packages
+
+# Read in data sources
+
+# Set data types for historical prices
+
+# Save clean historical prices Excel
+
+# Set data types for cash records
+
+# Save clean cash Excel
+
+# Set data types for portfolio transactions
+
+# Save clean portfolio transactions
