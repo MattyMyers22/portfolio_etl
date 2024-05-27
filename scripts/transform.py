46102305c6576d58566ace1b3884ee963df132f3
MattyMyers22 <mmyers2206@gmail.com>
2024-05-27 16:25:09 -0700

    data cleaning and clean excel save for cash data

--- scripts/transform.py
+++ scripts/transform.py
@@ -4,16 +4,18 @@
 # Author: Matthew Myers
 
 # Import packages
+import pandas as pd
 
 # Read in data sources
-
-# Set data types for historical prices
-
-# Save clean historical prices Excel
+portfolio_df = pd.read_excel('data/raw_portfolio.xlsx')
+cash_df = pd.read_excel('data/raw_cash.xlsx')
+prices_df = pd.read_excel('data/historical_prices.xlsx')
 
 # Set data types for cash records
+cash_df['date'] = pd.to_datetime(cash_df['date'])
 
 # Save clean cash Excel
+cash_df.to_excel('data/clean_cash.xlsx', index=False)
 
 # Set data types for portfolio transactions
 
