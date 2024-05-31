8bfdf050e401ce67c321c26b19f35b5ae130e7ca
MattyMyers22 <mmyers2206@gmail.com>
2024-05-31 16:36:44 -0700

    added MySQL connection

--- scripts/load.py
+++ scripts/load.py
@@ -5,5 +5,16 @@
 # Import parameters from config file
 from config import db_name, db_host, db_pwd, db_user
 
+# Import packages
+from sqlalchemy import create_engine, text
+
 # Construct the connection string
 connection_string = f"mysql+pymysql://{db_user}:{db_pwd}@{db_host}/{db_name}"
+
+# Create the engine
+engine = create_engine(connection_string)
+
+# Print a message to confirm connection
+print('Connected to MySQL database successfully!')
+
+# Target tables
