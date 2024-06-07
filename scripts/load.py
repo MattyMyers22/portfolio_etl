6d7a3b14a27d63108d0137dcfe9bf347158ce9a6
MattyMyers22 <mmyers2206@gmail.com>
2024-06-06 17:02:42 -0700

    Finalized first version of load script

--- scripts/load.py
+++ scripts/load.py
@@ -7,6 +7,7 @@ from config import db_name, db_host, db_pwd, db_user
 
 # Import packages
 from sqlalchemy import create_engine, text
+import pandas as pd
 
 # Construct the connection string
 connection_string = f"mysql+pymysql://{db_user}:{db_pwd}@{db_host}/{db_name}"
@@ -17,4 +18,22 @@ engine = create_engine(connection_string)
 # Print a message to confirm connection
 print('Connected to MySQL database successfully!')
 
-# Target tables
+# Read in data to load
+cash_df = pd.read_excel('data/clean_cash.xlsx')
+portfolio_df = pd.read_excel('data/clean_portfolio.xlsx')
+prices_df = pd.read_excel('data/historical_prices.xlsx')
+
+# Execute tasks
+with engine.connect() as conn:
+    # Load tables
+    cash_df.to_sql('cash', engine, index=False, if_exists='replace')
+    print('Loaded cash table \n')
+    portfolio_df.to_sql('portfolio', engine, index=False, if_exists='replace')
+    print('Loaded portfolio table \n')
+    prices_df.to_sql('prices', engine, index=False, if_exists='replace')
+    print('Loaded prices table \n')
+
+# Close engine
+engine.dispose()
+
+print('Disconnected from database')
