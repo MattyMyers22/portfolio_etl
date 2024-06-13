1d053b9abbf17661487e515fc7f64c829779af70
MattyMyers22 <mmyers2206@gmail.com>
2024-06-12 17:38:47 -0700

    change prices column names and save as clean excel

--- scripts/transform.py
+++ scripts/transform.py
@@ -9,6 +9,7 @@ import pandas as pd
 # Read in data sources
 portfolio_df = pd.read_excel('data/raw_portfolio.xlsx')
 cash_df = pd.read_excel('data/raw_cash.xlsx')
+prices_df = pd.read_excel('data/raw_prices.xlsx')
 
 # Set data types for cash records
 cash_df['date'] = pd.to_datetime(cash_df['date'])
@@ -22,3 +23,11 @@ portfolio_df['sell_date'] = pd.to_datetime(portfolio_df['sell_date'], format='mi
 
 # Save clean portfolio transactions
 portfolio_df.to_excel('data/clean_portfolio.xlsx', index=False)
+
+# Rename columns for historical prices
+new_names = {'Date':'date', 'Open':'open', 'High':'high', 'Low':'low', 'Close':'close',
+             'Adj Close':'adj_close', 'Volume':'volume'}
+prices_df = prices_df.rename(columns=new_names)
+
+# Save clean historical prices
+prices_df.to_excel('data/clean_prices.xlsx', index=False)
