ef13fd8f8b955e8b40ea49b1b9ff4f1459e0e2fe
MattyMyers22 <mmyers2206@gmail.com>
2024-06-15 16:24:13 -0700

    Fixed realized PL

--- scripts/portfolio_queries.sql
+++ scripts/portfolio_queries.sql
@@ -8,7 +8,7 @@ USE portfolio_dwh;
 WITH purchases AS (	
     SELECT symbol, purchase_date, purchase_price, SUM(shares) AS shares_purchased
 	FROM portfolio
-	WHERE account = 'Roth IRA' AND transaction_type IN ('buy', 'reinvestment')
+	WHERE account = 'Roth IRA' AND transaction_type = 'buy'
 	GROUP BY symbol, purchase_date, purchase_price),
 -- Selling history
 sellings AS (
@@ -24,7 +24,7 @@ SELECT p.symbol, p.purchase_date, p.purchase_price, shares_purchased,
     (shares_purchased - COALESCE(shares_sold, 0)) AS current_shares,
     purchase_price * shares_purchased AS cost_basis,
     shares_sold * sell_price AS realized_return,
-    (shares_sold * sell_price) - (purchase_price * shares_purchased) AS realized_pl
+    (shares_sold * sell_price) - (purchase_price * shares_sold) AS realized_pl
 FROM purchases AS p
 LEFT JOIN sellings AS s
 	USING(symbol, purchase_date, purchase_price);
