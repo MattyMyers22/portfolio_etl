63c00df545dd1cc26b1d2e0bf3111dd4ae208dba
MattyMyers22 <mmyers2206@gmail.com>
2024-06-15 17:21:24 -0700

    Added unrealized costs and returns

--- scripts/portfolio_queries.sql
+++ scripts/portfolio_queries.sql
@@ -6,10 +6,10 @@ USE portfolio_dwh;
 -- Get current holdings
 -- Purchase history
 WITH purchases AS (	
-    SELECT symbol, purchase_date, purchase_price, SUM(shares) AS shares_purchased
+    SELECT transaction_type, symbol, purchase_date, purchase_price, SUM(shares) AS shares_purchased
 	FROM portfolio
-	WHERE account = 'Roth IRA' AND transaction_type = 'buy'
-	GROUP BY symbol, purchase_date, purchase_price),
+	WHERE account = 'Roth IRA' AND transaction_type IN ('buy', 'reinvestment')
+	GROUP BY transaction_type, symbol, purchase_date, purchase_price),
 -- Selling history
 sellings AS (
 	SELECT symbol, purchase_date, purchase_price, SUM(shares) AS shares_sold, 
@@ -17,14 +17,61 @@ sellings AS (
 	FROM portfolio
     WHERE account = 'Roth IRA' AND transaction_type = 'sell'
     GROUP BY symbol, purchase_date, purchase_price, sell_date, sell_price
+),
+-- Get latest prices
+lates_prices AS (
+	SELECT symbol, date, adj_close
+	FROM prices
+	WHERE date = (SELECT MAX(date) FROM prices)
+	-- Add row for VFIAX
+	UNION
+	SELECT 'VFIAX' AS symbol,
+		(SELECT MAX(date) FROM prices) AS date,
+		-- Set VFIAX price equal to that for VOO
+		(SELECT adj_close 
+		FROM prices 
+		WHERE symbol = 'VOO' AND
+			date = (SELECT MAX(date) FROM prices)) AS adj_close
 )
 
-SELECT p.symbol, p.purchase_date, p.purchase_price, shares_purchased, 
-	shares_sold, sell_date, sell_price, 
+SELECT transaction_type, p.symbol, p.purchase_date, p.purchase_price, shares_purchased, 
+	shares_sold, sell_date, sell_price,
+    -- Current shares
     (shares_purchased - COALESCE(shares_sold, 0)) AS current_shares,
-    purchase_price * shares_purchased AS cost_basis,
+    -- Total cost basis, 0 if reinvested dividends
+    CASE
+		WHEN transaction_type = 'buy' THEN purchase_price * shares_purchased
+        WHEN transaction_type = 'reinvestment' THEN 0
+	END AS cost_basis,
+    -- Return from sold shares
     shares_sold * sell_price AS realized_return,
-    (shares_sold * sell_price) - (purchase_price * shares_sold) AS realized_pl
+    -- Profit/loss for shares sold
+    (shares_sold * sell_price) - (purchase_price * shares_sold) AS realized_pl,
+    -- Cost basis for unsold shares, 0 if reinvested dividends
+    CASE
+		WHEN transaction_type = 'buy' THEN (shares_purchased - COALESCE(shares_sold, 0)) * purchase_price
+        WHEN transaction_type = 'reinvestment' THEN 0
+	END AS unrealized_cost_basis,
+    -- Cost basis for sold shares, 0 if reinvested dividends
+    CASE
+		WHEN transaction_type = 'buy' THEN COALESCE(shares_sold, 0) * purchase_price
+        WHEN transaction_type = 'reinvestment' THEN 0
+	END AS realized_cost_basis,
+    -- Latest price
+    adj_close AS latest_price,
+    -- Unrealized return
+    adj_close * (shares_purchased - COALESCE(shares_sold, 0)) AS unrealized_return,
+    -- Unrealized profit/loss
+    (adj_close * (shares_purchased - COALESCE(shares_sold, 0))) - (
+		CASE
+			WHEN transaction_type = 'buy' THEN (shares_purchased - COALESCE(shares_sold, 0)) * purchase_price
+			WHEN transaction_type = 'reinvestment' THEN 0
+		END
+    ) AS unrealized_pl
 FROM purchases AS p
+-- Join sellings by symbol and purchase date and price
 LEFT JOIN sellings AS s
-	USING(symbol, purchase_date, purchase_price);
+	USING(symbol, purchase_date, purchase_price)
+-- Join for latest prices
+LEFT JOIN lates_prices AS l
+	USING(symbol);
