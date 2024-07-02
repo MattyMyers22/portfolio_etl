72e3c206b9acbeda7046296ef2ae79843ea255b0
MattyMyers22 <mmyers2206@gmail.com>
2024-07-01 21:25:41 -0700

    Fixed query for true current holdings list

--- scripts/portfolio_queries.sql
+++ scripts/portfolio_queries.sql
@@ -1,9 +1,32 @@
--- Create view for investment portfolio breakdown
-
 -- Connect to portfolio_dwh
 USE portfolio_dwh;
 
--- Get current holdings
+-- Create view for current holdings
+WITH purchases AS (	
+    SELECT symbol, purchase_date, purchase_price, SUM(shares) AS shares_purchased
+	FROM portfolio
+	WHERE account = 'Roth IRA' AND transaction_type IN ('buy', 'reinvestment')
+	GROUP BY symbol, purchase_date, purchase_price),
+-- Selling history
+sellings AS (
+	SELECT symbol, purchase_date, purchase_price, SUM(shares) AS shares_sold
+	FROM portfolio
+    WHERE account = 'Roth IRA' AND transaction_type = 'sell'
+    GROUP BY symbol, purchase_date, purchase_price),
+current AS (
+	SELECT p.symbol, p.purchase_date, p.purchase_price, shares_purchased, shares_sold,
+		shares_purchased - COALESCE(shares_sold, 0) AS current_shares
+	FROM purchases AS p
+	LEFT JOIN sellings AS s
+		USING(symbol, purchase_date, purchase_price))
+        
+SELECT symbol, SUM(current_shares)
+FROM current
+GROUP BY symbol
+HAVING SUM(current_shares) <> 0;
+
+/*
+-- Get full returns history
 -- Purchase history
 WITH purchases AS (	
     SELECT transaction_type, symbol, purchase_date, purchase_price, SUM(shares) AS shares_purchased
@@ -75,3 +98,6 @@ LEFT JOIN sellings AS s
 -- Join for latest prices
 LEFT JOIN lates_prices AS l
 	USING(symbol);
+*/
+
+SELECT * FROM prices;
