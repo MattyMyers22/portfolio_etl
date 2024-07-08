927e73c6484b693ab0a8a478c8c1837e9e8af74e
MattyMyers22 <mmyers2206@gmail.com>
2024-07-07 17:33:40 -0700

    brought in total cost basis and round functions

--- scripts/portfolio_queries.sql
+++ scripts/portfolio_queries.sql
@@ -2,6 +2,7 @@
 USE portfolio_dwh;
 
 -- Create view for current holdings
+-- Purchase history
 WITH purchases AS (	
     SELECT symbol, purchase_date, purchase_price, SUM(shares) AS shares_purchased
 	FROM portfolio
@@ -13,21 +14,25 @@ sellings AS (
 	FROM portfolio
     WHERE account = 'Roth IRA' AND transaction_type = 'sell'
     GROUP BY symbol, purchase_date, purchase_price),
+-- Find current share counts
 current AS (
 	SELECT p.symbol, p.purchase_date, p.purchase_price, shares_purchased, shares_sold,
 		shares_purchased - COALESCE(shares_sold, 0) AS current_shares
 	FROM purchases AS p
 	LEFT JOIN sellings AS s
 		USING(symbol, purchase_date, purchase_price)),
+-- Current holdings with share counts
 holdings AS (
 	SELECT symbol, SUM(current_shares) AS shares
 	FROM current
 	GROUP BY symbol
 	HAVING SUM(current_shares) <> 0),
+-- Find cost basis for current holdings
 basis AS (
-	SELECT h.symbol, shares, SUM(unrealized_cost_basis) AS unrealized_cost_basis, 
-		SUM(realized_cost_basis) AS realized_cost_basis, 
-		ROUND((SUM(unrealized_cost_basis) / shares), 2) AS avg_unr_cost_basis
+	SELECT h.symbol, shares, ROUND(SUM(unrealized_cost_basis), 2) AS unrealized_cost_basis, 
+		ROUND(SUM(realized_cost_basis), 2) AS realized_cost_basis, 
+		ROUND((SUM(unrealized_cost_basis) / shares), 2) AS avg_unr_cost_basis,
+        ROUND(SUM(COALESCE(unrealized_cost_basis, 0)) + SUM(COALESCE(realized_cost_basis, 0)), 2) AS total_cost_basis
 	FROM holdings AS h
 	INNER JOIN
 		(SELECT symbol, (purchase_price * current_shares) AS unrealized_cost_basis,
@@ -36,9 +41,10 @@ basis AS (
 	USING(symbol)
 	GROUP BY h.symbol)
     
-SELECT b.symbol, shares, adj_close AS latest_price, (shares * adj_close) AS value, 
+SELECT b.symbol, shares, adj_close AS latest_price, ROUND((shares * adj_close), 2) AS value, 
 	avg_unr_cost_basis, unrealized_cost_basis,
-    (shares * adj_close) - unrealized_cost_basis AS unrealized_pl, realized_cost_basis
+    ROUND((shares * adj_close) - unrealized_cost_basis, 2) AS unrealized_pl, realized_cost_basis,
+    total_cost_basis
 FROM basis AS b
 INNER JOIN
 	(SELECT *
